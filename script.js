7a83e64345b088e97978a0fd3c010255eaa707de
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-01 22:30:12 -0500

    Add remove function

--- script.js
+++ script.js
@@ -1,39 +1,71 @@
-const collectionBooks = [];
+let collectionBooks = [];
 
 // const form = document.querySelector('#bookForm');
-const booksContainer = document.querySelector('#container-book');
+const title = document.querySelector('#input-title');
+const author = document.querySelector('#input-author');
 // const button = document.querySelector('#add-book');
+var position = -1;
 
-class NewBook {
-  constructor(title, author) {
+
+class newBook {
+  constructor (title,author,index){
     this.title = title;
     this.author = author;
+    this.index = index;
   }
 }
 
+
+
 // check with every letter clicked
-function saveInfo() {
-  const newtitle = document.querySelector('#input-title').value;
-  const newauthor = document.querySelector('#input-author').value;
+function saveInfo () {
+  let newtitle = document.querySelector('#input-title').value;
+  let newauthor = document.querySelector('#input-author').value;
+  
+  // Counter to define the index in the colections books
+  position++;
 
+  // If the title or the author is empty, then don't add the book
+  if( newtitle === '' || newauthor === '') {
+    return null;
+  } else {
   // Create a new Book
-  const bookInfo = new NewBook(newtitle, newauthor);
-
-  // Push the book inside the collection
+  let bookInfo = new newBook ( newtitle , newauthor, position );
   collectionBooks.push(bookInfo);
+  }
 
-  const addingBooks = () => {
-    const booksCards = '';
-
-    document.getElementById('container-book').innerHTML = collectionBooks.map((items) => `<div id="cards">
-      <h3>${items.title}</h3>
-      <p>${items.author}</p>
-      <button id="remove">Remove</button>
-      <hr>
-      </div>`).join('');
-
-    booksContainer.insertAdjacentHTML('beforeend', booksCards);
-  };
-  return addingBooks();
+  // Print in the html file 
+  printf(collectionBooks);
+  return collectionBooks;
 }
+
+function deletInfo(input) {
+  console.log(collectionBooks);
+  { console.log(input)};
+
+   collectionBooks = collectionBooks.filter((collectionBooks)=> {
+      if(collectionBooks.index === input ){ 
+        console.log('Eliminado:', input)
+        position--;
+      } else {
+        return true;
+      }
+   });
+  printf(collectionBooks);
+  return collectionBooks;
+}
+
+function printf(collectionBooks){
+  document.getElementById('container-book').innerHTML = collectionBooks.map((items) => 
+  `<div id="cards">
+    <h3>${items.title}</h3>
+    <p>${items.author}</p>
+    <button onclick="deletInfo(${items.index})">Remove</button>
+    <hr>
+    </div>`
+).join('');
+}
+
+printf();
 saveInfo();
+deletInfo();
