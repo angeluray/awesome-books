bc00663d9e810302335e96c96aa776ce88676cd0
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-02 20:05:49 -0500

    Add classes and method to add and delet

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 let collectionBooks = [];
-
-
-const title = document.querySelector('#input-title');
-const author = document.querySelector('#input-author');
-
 let position = -1;
 
 // Classe NewBook and the constructor 
@@ -13,31 +8,20 @@ class NewBook {
     this.author = author;
     this.index = index;
   }
-}
 
-// Print the info in the browser
-function printf(collectionBooks) {
-  document.getElementById('container-book').innerHTML = collectionBooks.map((items) => 
-  `
-  <div id="cards">
-  <p>"${items.title}" by ${items.author} </p>
-  <button class="buttonRemove" onclick="deletInfo(${items.index})">Remove</button>
-  </div>
-`).join('');
-}
-
-// check with every letter clicked
-function saveInfo() {
+  // ADD book
+  static saveInfo() {
   const newtitle = document.querySelector('#input-title').value;
   const newauthor = document.querySelector('#input-author').value;
+
+   // If the title or the author is empty, then don't add the book
+  if (newtitle === '' || newauthor === '') {
+    return null;
+  }
 
   // Counter to define the index in the colections books
   position += 1;
 
-  // If the title or the author is empty, then don't add the book
-  if (newtitle === '' || newauthor === '') {
-    return null;
-  }
   // Create a new Book
   const bookInfo = new NewBook(newtitle, newauthor, position);
   collectionBooks.push(bookInfo);
@@ -45,21 +29,17 @@ function saveInfo() {
   // Print in the html file
   printf(collectionBooks);
   return collectionBooks;
-}
+  }
 
-// Local Storage
-const inputsObject = {};
-document.querySelector('#bookForm').addEventListener('submit', () => {
-  inputsObject.title = title.value;
-  inputsObject.author = author.value;
-  localStorage.setItem(title.value, JSON.stringify(inputsObject));
-});
-
-// Delete the book selected
-function deletInfo(input) {
+  // DELET book
+  static deletInfo(input) {
   collectionBooks = collectionBooks.filter((collectionBooks) => {
+    let num = 0;
     if (collectionBooks.index === input) {
-      position -= 1;
+      if(num == 0) {
+        position -= 1;
+        num += 1; 
+      }
     } else {
       return true;
     }
@@ -67,8 +47,39 @@ function deletInfo(input) {
   });
   printf(collectionBooks);
   return collectionBooks;
+  }
 }
 
-printf();
-saveInfo();
-deletInfo();
+// Print the info in the browser
+function printf(input) {
+  document.getElementById('container-book').innerHTML = input.map((items) => 
+  `
+  <div id="cards">
+  <p>"${items.title}" by ${items.author} </p>
+  <button class="buttonRemove" onclick="NewBook.deletInfo(${items.index})">Remove</button>
+  </div>
+  `
+).join('');
+}
+
+// Local Storage
+const box = [];
+const inputsObject = {};
+
+document.querySelector('#bookForm').addEventListener('submit', () => {
+
+  const lsTitle = document.querySelector('#input-title');
+  const lsAuthor = document.querySelector('#input-author');
+
+  inputsObject.title = lsTitle.value;
+  inputsObject.author = lsAuthor.value;
+  box.push(inputsObject);
+
+  localStorage.setItem('data', JSON.stringify(inputsObject));
+  console.log('Caja', box);
+});
+
+
+
+
+
